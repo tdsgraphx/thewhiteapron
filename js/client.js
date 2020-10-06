76801bebdd715aa6a01f8f09c3a127af5dda8ff8
TDS Graphics <54712534+tdsgraphx@users.noreply.github.com>
2020-10-06 15:41:32 +0200

    Add files via upload
    initial commit

---
+++ js/client.js
@@ -0,0 +1,54 @@
+// JavaScript Document
+
+     $(document).ready(function() {
+
+       $("div#newDiv").click(function() {
+
+            alert("Hello world!");
+
+          });
+
+    });
+
+	 $(document).ready(function() {
+
+       $("div#lastDiv").click(function() {
+
+            alert("Fuck you!");
+
+          });
+
+    });
+
+     $(document).ready(function() {
+
+       $("div#main").click(function() {
+
+            alert("Hello world!");
+
+          });
+
+    });
+
+	 $(document).ready(function() {
+
+       $("div#footer").click(function() {
+
+            alert("Fuck you!");
+
+          });
+
+    });
+
+	 $(document).ready(function() { 
+			  
+		 $("#div2").css("background-color", "yellow"); 
+			 
+    }); 
+
+     $(document).ready(function() { 
+			  
+        $("#div3").css("background-color", "green"); 
+			 
+   }); 
+
